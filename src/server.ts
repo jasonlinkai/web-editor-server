0c59cbc8e161136aba3992f2f45f8f82b675e227
Jacky <jacky@iyes.dev>
2024-05-16 18:41:23 +0800

    feat: upload api

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,65 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import cors, { CorsOptions } from "cors";
+import fileUpload from "express-fileupload";
 import path from "path";
+import { v4 } from "uuid";
+import fs from "fs";
+
+const paths = {
+  public: path.join(__dirname, "../", "./public"),
+  uploads: path.join(__dirname, "../", "./public", "./uploads"),
+};
 
 export default class Server {
   constructor(app: Application) {
     this.config(app);
-    this.register(app)
+    this.register(app);
   }
 
   private config(app: Application): void {
     const corsOptions: CorsOptions = {
-      origin: "http://localhost:3001"
+      origin: "http://localhost:3000",
     };
 
     app.use(cors(corsOptions));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    app.use('/public', express.static(path.join(__dirname, '../', 'public')));
+    app.use(fileUpload());
+    app.use("/public", express.static(paths.public));
   }
 
   private register(app: Application): void {
-    app.get('/', (req: Request, res: Response) => {
-      res.send('This is a public route!');
-  });
+    app.get("/uploaded-images", (req, res) => {
+
+      const files = fs.readdirSync(paths.uploads);
+      const fileNames = files.map(file => {
+        return path.basename(file);
+      });
+
+      res.send({
+        code: 0,
+        message: 'success',
+        data: fileNames,
+      });
+    });
+    app.post("/upload", (req, res) => {
+      if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send("No files were uploaded.");
+      }
+      const uploadedFile = req.files.file as fileUpload.UploadedFile;
+      const ext = uploadedFile.name.split(".")[1];
+      const newFileName = v4() + `.${ext}`;
+      const savePath = `${paths.uploads}/${newFileName}`;
+      uploadedFile.mv(savePath, (err) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        res.send({
+          code: 0,
+          message: 'success',
+          data: [newFileName],
+        });
+      });
+    });
   }
 }
